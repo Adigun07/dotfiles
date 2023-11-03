13c0e61b43d73d5e40bd7c0d0397f011d8dce59d
Adigun07 <abdussomodadigun@gmail.com>
2023-11-03 22:29:21 +0100

    added a custom command to change nvim-colorizer highlight mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -132,6 +132,32 @@ local plugins = {
 				-- all the sub-options of filetypes apply to buftypes
 				buftypes = {},
 			})
+			vim.api.nvim_create_user_command("ColorizerMode", function(opts)
+				require("colorizer").setup({
+					filetypes = { "*" },
+					user_default_options = {
+						RGB = true, -- #RGB hex codes
+						RRGGBB = true, -- #RRGGBB hex codes
+						names = true, -- "Name" codes like Blue or blue
+						RRGGBBAA = true, -- #RRGGBBAA hex codes
+						AARRGGBB = true, -- 0xAARRGGBB hex codes
+						rgb_fn = true, -- CSS rgb() and rgba() functions
+						hsl_fn = true, -- CSS hsl() and hsla() functions
+						css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+						css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+						mode = opts.fargs[1], -- Set the display mode.
+						virtualtext = "■",
+					},
+					-- all the sub-options of filetypes apply to buftypes
+					buftypes = {},
+				})
+				vim.cmd.ColorizerToggle()
+			end, {
+				nargs = 1,
+				complete = function(ArgLead, CmdLine, CursorPos)
+					return { "foreground", "background", "virtualtext" }
+				end,
+			})
 		end,
 	},
 
