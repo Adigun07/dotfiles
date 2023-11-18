08731398d4444a8527ece86ee0fa22b81d0610e3
Adigun07 <abdussomodadigun@gmail.com>
2023-11-18 10:24:11 +0100

    changed formatting keybinds

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -39,9 +39,6 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>n", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<space>f", function()
-		vim.lsp.buf.format({ async = true })
-	end, bufopts)
 end
 
 require("neodev").setup({})
