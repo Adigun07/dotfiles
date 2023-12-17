6b76c9dde8067468d57e487399712d6133108615
Adigun07 <abdussomodadigun@gmail.com>
2023-12-17 17:41:03 +0100

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,6 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-vim.opt.foldcolumn = "1"
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 20
